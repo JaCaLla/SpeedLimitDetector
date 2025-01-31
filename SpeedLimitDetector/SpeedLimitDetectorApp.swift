cb9f2822c06678624616e51c5b4a48994fcb8310
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-01-31 12:47:39 +0100

    Blank ios project

---
+++ SpeedLimitDetector/SpeedLimitDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedLimitDetectorApp.swift
+//  SpeedLimitDetector
+//
+//  Created by Javier Calatrava on 31/1/25.
+//
+
+import SwiftUI
+
+@main
+struct SpeedLimitDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
